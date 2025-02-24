9664f13843ba7c8d5c82e755e712fdc09a7111b9
mohamed mustafa <mohamedmustafa8112001@gmail.com>
2025-02-24 15:59:44 +0200

    first

---
+++ Day1/main.py
@@ -0,0 +1,92 @@
+# program 1
+
+# print("hello")
+# count = 0
+# str = input('Enter the name:')
+# chars=['a','e','i','o','u']
+# for c in chars:
+#     for ch in str:  
+#         if( c == ch ):
+#             count=count+1
+# print(f" the number of vowels in {str} : {count}")
+
+#program 2
+
+# def fun1(length,start):
+#     numbers=[]
+#     num=0
+#     while num<length:
+#         numbers.append(start+num)
+#         num+=1
+#     print(numbers)
+
+
+# fun1(10,5)
+
+
+
+
+# program 3
+
+# numbers=[]
+# num = 0
+# while num<5:
+#     elm = input(f"enter the elm {num} : ")
+#     numbers.append(int(elm))
+#     num+=1
+
+# print(numbers)
+# numbers.sort(reverse=True) 
+# print(numbers)   
+
+# numbers.sort(reverse=False) 
+# print(numbers)
+
+# program 4
+
+# def fun():
+#     num = int(input("enter the number : "))
+
+#     if ( num%3==0 and num%5==0 ):
+#         print("FizzBuzz")
+#     elif (num%5==0):
+#         print("Buzz")
+#     elif(num%3==0):
+#         print("Fizz")
+        
+# fun()
+
+
+# proplem 5
+
+# def fun():
+#     str = input("enter the name : ")
+#     str1 = str[::-1]
+#     print(str)
+#     print(str1)
+
+
+# fun()
+
+
+# proplem 6
+# import math
+
+# def fun():
+#     radius = int(input("enter the radius : "))
+#     area = math.pi * math.pow(radius,2)
+#     area = math.pi * (radius ** 2)
+#     Circumference = math.pi * 2 * radius
+
+#     print(f"Area of circle : {area}")
+#     print(f"Circumference of circle : {Circumference}")
+
+# fun()
+
+# program 7
+
+def fun():
+    name = input("enter your name: ")
+    if isinstance(name,int) or (isinstance(name,str)and name.s:
+
+    
